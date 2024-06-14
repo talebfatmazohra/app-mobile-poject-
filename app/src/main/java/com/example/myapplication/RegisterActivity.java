478a3f477a910d1e235efd075ba41cb1c626d426
talebfatmazohra <fatmazohrami@gmail.com>
2024-06-14 20:35:31 +0200

    voici qu'est ce que j'ai fait jusqu'a maintenant j'attend attentivemeant votre remarque

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -1,14 +1,14 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
+//import android.widget.EditText;
+//import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+//import androidx.core.graphics.Insets;
+//import androidx.core.view.ViewCompat;
+//import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
 
